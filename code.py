f4285a32fc559f01b8a3fd6b94f1dbf9000f476b
Tooba Jamal <52610124+ToobaJamal@users.noreply.github.com>
2022-01-31 18:03:39 +0500

    Update code.py

--- code.py
+++ code.py
@@ -1,9 +1,13 @@
+def emoji_conv(message):
+    words = message.split(" ")
+    emojis = {':)': '🙂',
+             ':(': '😔',
+             '<3': '❤️'}
+    output = ""
+    for word in words:
+        output = output + emojis.get(word, word) + " "
+    return output
+
+
 message = input(">")
-words = message.split(" ")
-emojis = {':)': '🙂',
-         ':(': '😔',
-         '<3': '❤️'}
-output = ""
-for word in words:
-    output = output + emojis.get(word, word) + " "
-print(output)
+print(emoji_conv(message))
